12befaca5574682943cc8889ba7fe247e6fdecd5
minkgkyaw <minkgkyaw1999@gmail.com>
2022-10-06 07:34:59 +0630

    fix: remove console.log

--- src/components/todo/TodoList.component.jsx
+++ src/components/todo/TodoList.component.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import TodoItem from "./Todo.component";
-import {Skeleton} from '../'
+import { Skeleton } from "../";
 
 const TodoList = ({ todos, deleteTodo, updateTodo }) => {
-  console.log(todos)
   return (
     <div className="todo-list">
-      {
-        todos.length !== 0 ? todos.map(({id, title, bgColor}) => <TodoItem key={id} id={id} title={title} deleteTodo={deleteTodo} updateTodo={updateTodo} bgColor={bgColor} />) : <Skeleton />
-      }
+      {todos.length !== 0 ? (
+        todos.map(({ id, title, bgColor }) => (
+          <TodoItem
+            key={id}
+            id={id}
+            title={title}
+            deleteTodo={deleteTodo}
+            updateTodo={updateTodo}
+            bgColor={bgColor}
+          />
+        ))
+      ) : (
+        <Skeleton />
+      )}
     </div>
   );
 };
